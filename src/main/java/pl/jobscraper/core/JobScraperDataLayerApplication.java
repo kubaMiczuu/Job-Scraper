81c6f7ae05fe40d6478adb4c2d25e89b0e374819
Dominik Smoszna <dominiksmoszna@gmail.com>
2026-01-28 20:49:33 +0100

    Initial commit - Spring Boot setup

---
+++ src/main/java/pl/jobscraper/core/JobScraperDataLayerApplication.java
@@ -0,0 +1,13 @@
+package pl.jobscraper.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobScraperDataLayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobScraperDataLayerApplication.class, args);
+    }
+
+}
